ee4d050f1faa9c61dd47824e67aa09a56d870759
HokageClient <75934966+HokageClient@users.noreply.github.com>
2025-07-19 01:08:41 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -91,4 +92,6 @@ def webhook():
     return 'ok'
 
 if __name__ == '__main__':
-    app.run(port=5000) 
+    # Исправлено для работы на Render
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
